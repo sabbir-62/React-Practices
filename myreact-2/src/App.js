559e87071cb4452feadf3a5d3d9079d11a6f1980
sabbir-62 <sabbir.hossain.se62@gmail.com>
2022-12-25 23:36:52 +0600

    myreact-2 added

---
+++ myreact-2/src/App.js
@@ -0,0 +1,11 @@
+import Clock from "./components/Clock"
+
+function App() {
+  return (
+    <>
+    <div className="App"><Clock/></div>
+    <div className="App"><Clock/></div></>
+  );
+}
+
+export default App;
